dacc4b84861dad4413da0b44c77d682eaf8c7963
nzivo <johnnnzivo@gmail.com>
2020-07-28 16:48:14 +0300

    Sign In UI Implemented

--- app/src/main/java/org/satima/sbooks/LoginActivity.java
+++ app/src/main/java/org/satima/sbooks/LoginActivity.java
@@ -2,13 +2,29 @@ package org.satima.sbooks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button CallSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        CallSignUp = findViewById(R.id.sign_up_screen);
+
+        CallSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
