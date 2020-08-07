5e9ab742f09f1c82f050b6fea87b2cb90f98ba0f
nzivo <johnnnzivo@gmail.com>
2020-08-07 08:33:32 +0300

    Connection constants

---
+++ app/src/main/java/org/satima/sbooks/Constant.java
@@ -0,0 +1,8 @@
+package org.satima.sbooks;
+
+public class Constant {
+    public static final String URL = "http://192.168.43.109:80/";
+    public static final String HOME = URL+"api";
+    public static final String LOGIN = HOME+"/login";
+    public static final String REGISTER = HOME+"/register";
+}
